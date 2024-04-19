aee299d2dda11c8581a808828d6c4f25741b04a0
Aliaskar Urakov <urakov.aliaskar@gmail.com>
2024-04-19 01:57:01 -0700

    added router, code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import Counter from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const App = () => {
   return (
     <div className='app'>
-      dhjskdjdlka
-      <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+
+          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };

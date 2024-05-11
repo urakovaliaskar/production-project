981660bdd359246b1d3b1d2433f8be5e52465c62
Aliaskar Urakov <urakov.aliaskar@gmail.com>
2024-05-10 21:53:19 -0700

    test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './classsNames';
+
+describe('classNames', () => {
+  test('with one param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional params', () => {
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe('someClass class1 class2');
+  });
+
+  test('with mods', () => {
+    expect(classNames(
+      'someClass',
+      {
+        hovered: true,
+        scrollable: true,
+      },
+      ['class1', 'class2'],
+    )).toBe('someClass class1 class2 hovered scrollable');
+    expect(classNames(
+      'someClass',
+      {
+        hovered: true,
+        scrollable: false,
+      },
+      ['class1', 'class2'],
+    )).toBe('someClass class1 class2 hovered');
+
+    expect(classNames(
+      'someClass',
+      {
+        hovered: true,
+        scrollable: undefined,
+      },
+      ['class1', 'class2'],
+    )).toBe('someClass class1 class2 hovered');
+  });
+});

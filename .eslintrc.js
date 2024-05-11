90e73c4793fabb4e860ef7ce21dce47b8fd933e0
Aliaskar Urakov <urakov.aliaskar@gmail.com>
2024-05-10 21:55:43 -0700

    ignore comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': [2, {'ignoreComments': true}]
     },
     globals: {
         __IS_DEV__: true,

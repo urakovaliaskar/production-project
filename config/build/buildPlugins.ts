3adb1155e48f991a3e6c42556c1ab76da1b592ee
Aliaskar Urakov <urakov.aliaskar@gmail.com>
2025-02-03 14:07:20 -0800

    bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,27 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buidPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
-
+export function buidPlugins({
+  paths
+}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
       template: paths.html
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css"
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
     }),
-    new webpack.DefinePlugin({ 
+    new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(true)
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
   ];
 }
--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -1,7 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classsNames';
-import cls from './PageError.module.scss'
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui/Button/ui/Button';
+import cls from './PageError.module.scss';
 
 interface IPageErrorProps {
   className?: string
@@ -11,15 +11,16 @@ export const PageError = ({ className }: IPageErrorProps) => {
   const { t } = useTranslation();
 
   const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
     location.reload();
-  }
+  };
 
   return (
     <div className={classNames(cls.PageError, {}, [className])}>
       <p>{t('page_error')}</p>
-      <Button onClick={reloadPage }>
+      <Button onClick={reloadPage}>
         {t('page_refresh')}
       </Button>
     </div>
-  )
-}; 
+  );
+};
